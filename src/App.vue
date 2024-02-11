fd366405288fea0d83323383fca6273f7b1598ad
Nemanja Malesija <nemanja.malesija@njuskalo.hr>
2024-02-11 21:04:16 +0100

    Move data to component through created and mapState

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<template></template>
+<template>
+  <Component2 />
+</template>
 <script>
+import Component2 from "./Component-2.vue";
 import useFakeStore1 from "./stores/store-1";
 
 const pageStore = useFakeStore1("fakeStore1");
 
 export default {
-  components: {},
+  components: { Component2 },
   data() {
     return {
       pageData: null,
@@ -21,6 +24,8 @@ export default {
 
   async mounted() {
     this.pageData = await this.fetchData();
+
+    console.log("Comming from component 1 (app)");
     console.log(this.pageData);
   },
 };
---
+++ src/Component-2.vue
@@ -0,0 +1,22 @@
+<template></template>
+<script>
+import useFakeStore1 from "./stores/store-1";
+import { mapState } from "pinia";
+
+const usePageStore = useFakeStore1("fakeStore1");
+
+export default {
+  pageStores: [usePageStore],
+
+  computed: {
+    ...mapState(usePageStore, ["pageData"]),
+  },
+
+  async created() {
+    await usePageStore().getData();
+
+    console.log("comming from component 2");
+    console.log(this.pageData);
+  },
+};
+</script>
